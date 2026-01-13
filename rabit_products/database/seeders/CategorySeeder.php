11c53737dbfe92a27c5adf460f9e77a35870f636
duyd486 <duyd486@gmail.com>
2026-01-13 08:42:04 +0700

    fix category seeder

--- rabit_products/database/seeders/CategorySeeder.php
+++ rabit_products/database/seeders/CategorySeeder.php
@@ -17,28 +17,28 @@ class CategorySeeder extends Seeder
             [
                 'name'=>'Sổ tay',
                 'parent_id'=>0,
-                'image_url'=>'http://127.0.0.1:8000/category_backgrounds/Sổ tay.jpg',
+                'thumbnail_url'=>'http://127.0.0.1:8000/category_backgrounds/Sổ tay.jpg',
                 'created_at'=>now(),
                 'updated_at'=>now()
             ],
             [
                 'name'=>'Vở viết',
                 'parent_id'=>0,
-                'image_url'=>'http://127.0.0.1:8000/category_backgrounds/Vở viết.jpg',
+                'thumbnail_url'=>'http://127.0.0.1:8000/category_backgrounds/Vở viết.jpg',
                 'created_at'=>now(),
                 'updated_at'=>now()
             ],
             [
                 'name'=>'Phụ kiện',
                 'parent_id'=>0,
-                'image_url'=>'http://127.0.0.1:8000/category_backgrounds/Phụ kiện.jpg',
+                'thumbnail_url'=>'http://127.0.0.1:8000/category_backgrounds/Phụ kiện.jpg',
                 'created_at'=>now(),
                 'updated_at'=>now()
             ],
             [
                 'name'=>'Art and Craft',
                 'parent_id'=>0,
-                'image_url'=>'http://127.0.0.1:8000/category_backgrounds/Art and Craft.jpg',
+                'thumbnail_url'=>'http://127.0.0.1:8000/category_backgrounds/Art and Craft.jpg',
                 'created_at'=>now(),
                 'updated_at'=>now()
             ],
@@ -48,35 +48,35 @@ class CategorySeeder extends Seeder
             [
                 'name'=>'Sổ kẻ ngang - Lined',
                 'parent_id'=>1,
-                'image_url'=>'http://127.0.0.1:8000/category_backgrounds/Sổ kẻ ngang - Lined.jpg',
+                'thumbnail_url'=>'http://127.0.0.1:8000/category_backgrounds/Sổ kẻ ngang - Lined.jpg',
                 'created_at'=>now(),
                 'updated_at'=>now()
             ],
             [
                 'name'=>'Sổ chấm - Dotgrid',
                 'parent_id'=>1,
-                'image_url'=>'http://127.0.0.1:8000/category_backgrounds/Sổ chấm - Dotgrid.jpg',
+                'thumbnail_url'=>'http://127.0.0.1:8000/category_backgrounds/Sổ chấm - Dotgrid.jpg',
                 'created_at'=>now(),
                 'updated_at'=>now()
             ],
             [
                 'name'=>'Sổ trơn - Plain',
                 'parent_id'=>1,
-                'image_url'=>'http://127.0.0.1:8000/category_backgrounds/Sổ trơn - Plain.jpg',
+                'thumbnail_url'=>'http://127.0.0.1:8000/category_backgrounds/Sổ trơn - Plain.jpg',
                 'created_at'=>now(),
                 'updated_at'=>now()
             ],
             [
                 'name'=>'Sổ ô vuông - Grid',
                 'parent_id'=>1,
-                'image_url'=>'http://127.0.0.1:8000/category_backgrounds/Sổ ô vuông - Grid.jpg',
+                'thumbnail_url'=>'http://127.0.0.1:8000/category_backgrounds/Sổ ô vuông - Grid.jpg',
                 'created_at'=>now(),
                 'updated_at'=>now()
             ],
             [
                 'name'=>'Sổ vẽ - Sketchbook',
                 'parent_id'=>1,
-                'image_url'=>'http://127.0.0.1:8000/category_backgrounds/Sổ vẽ - Sketchbook.jpg',
+                'thumbnail_url'=>'http://127.0.0.1:8000/category_backgrounds/Sổ vẽ - Sketchbook.jpg',
                 'created_at'=>now(),
                 'updated_at'=>now()
             ]
@@ -85,35 +85,35 @@ class CategorySeeder extends Seeder
             [
                 'name'=>'Vở kẻ ngang - Lined',
                 'parent_id'=>2,
-                'image_url'=>'http://127.0.0.1:8000/category_backgrounds/Vở kẻ ngang - Lined.jpg',
+                'thumbnail_url'=>'http://127.0.0.1:8000/category_backgrounds/Vở kẻ ngang - Lined.jpg',
                 'created_at'=>now(),
                 'updated_at'=>now()
             ],
             [
                 'name'=>'Vở chấm - Dotgrid',
                 'parent_id'=>2,
-                'image_url'=>'http://127.0.0.1:8000/category_backgrounds/Vở chấm - Dotgrid.jpg',
+                'thumbnail_url'=>'http://127.0.0.1:8000/category_backgrounds/Vở chấm - Dotgrid.jpg',
                 'created_at'=>now(),
                 'updated_at'=>now()
             ],
             [
                 'name'=>'Vở ô vuông - Grid',
                 'parent_id'=>2,
-                'image_url'=>'http://127.0.0.1:8000/category_backgrounds/Vở ô vuông - Grid.jpg',
+                'thumbnail_url'=>'http://127.0.0.1:8000/category_backgrounds/Vở ô vuông - Grid.jpg',
                 'created_at'=>now(),
                 'updated_at'=>now()
             ],
             [
                 'name'=>'Vở siêu dày - Composition',
                 'parent_id'=>2,
-                'image_url'=>'http://127.0.0.1:8000/category_backgrounds/Vở siêu dày - Composition.jpg',
+                'thumbnail_url'=>'http://127.0.0.1:8000/category_backgrounds/Vở siêu dày - Composition.jpg',
                 'created_at'=>now(),
                 'updated_at'=>now()
             ],
             [
                 'name'=>'Vở Cornell - Cornell',
                 'parent_id'=>2,
-                'image_url'=>'http://127.0.0.1:8000/category_backgrounds/Vở Cornell - Cornell.jpg',
+                'thumbnail_url'=>'http://127.0.0.1:8000/category_backgrounds/Vở Cornell - Cornell.jpg',
                 'created_at'=>now(),
                 'updated_at'=>now()
             ]
@@ -122,28 +122,28 @@ class CategorySeeder extends Seeder
             [
                 'name'=>'Bút',
                 'parent_id'=>3,
-                'image_url'=>'http://127.0.0.1:8000/category_backgrounds/Bút.jpg',
+                'thumbnail_url'=>'http://127.0.0.1:8000/category_backgrounds/Bút.jpg',
                 'created_at'=>now(),
                 'updated_at'=>now()
             ],
             [
                 'name'=>'Sticker',
                 'parent_id'=>3,
-                'image_url'=>'http://127.0.0.1:8000/category_backgrounds/Sticker.jpg',
+                'thumbnail_url'=>'http://127.0.0.1:8000/category_backgrounds/Sticker.jpg',
                 'created_at'=>now(),
                 'updated_at'=>now()
             ],
             [
                 'name'=>'Washitape',
                 'parent_id'=>3,
-                'image_url'=>'http://127.0.0.1:8000/category_backgrounds/Washitape.jpg',
+                'thumbnail_url'=>'http://127.0.0.1:8000/category_backgrounds/Washitape.jpg',
                 'created_at'=>now(),
                 'updated_at'=>now()
             ],
             [
                 'name'=>'Giấy note',
                 'parent_id'=>3,
-                'image_url'=>'http://127.0.0.1:8000/category_backgrounds/Giấy note.jpg',
+                'thumbnail_url'=>'http://127.0.0.1:8000/category_backgrounds/Giấy note.jpg',
                 'created_at'=>now(),
                 'updated_at'=>now()
             ],
@@ -152,21 +152,21 @@ class CategorySeeder extends Seeder
             [
                 'name'=>'Đất sét tự khô - Air Dry Clay',
                 'parent_id'=>4,
-                'image_url'=>'http://127.0.0.1:8000/category_backgrounds/Đất sét tự khô - Air Dry Clay.jpg',
+                'thumbnail_url'=>'http://127.0.0.1:8000/category_backgrounds/Đất sét tự khô - Air Dry Clay.jpg',
                 'created_at'=>now(),
                 'updated_at'=>now()
             ],
             [
                 'name'=>'Luyện chữ - Calligraphy',
                 'parent_id'=>4,
-                'image_url'=>'http://127.0.0.1:8000/category_backgrounds/Luyện chữ - Calligraphy.jpg',
+                'thumbnail_url'=>'http://127.0.0.1:8000/category_backgrounds/Luyện chữ - Calligraphy.jpg',
                 'created_at'=>now(),
                 'updated_at'=>now()
             ],
             [
                 'name'=>'Luyện chữ - Handwriting',
                 'parent_id'=>4,
-                'image_url'=>'http://127.0.0.1:8000/category_backgrounds/Luyện chữ - Handwriting.jpg',
+                'thumbnail_url'=>'http://127.0.0.1:8000/category_backgrounds/Luyện chữ - Handwriting.jpg',
                 'created_at'=>now(),
                 'updated_at'=>now()
             ],
